6c412efc956a10c3d6172900000df15c49a59df4
liuke <18162327131@163.com>
2022-11-24 21:18:35 +0800

    feat:

--- leetcode/main.go
+++ leetcode/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	//fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
 	//fmt.Println(romanToInt("MCDLXXVI"))
@@ -7,5 +9,6 @@ func main() {
 	//[[75,21,13,24,8],[24,100,40,49,62]]
 	//fmt.Println(diagonalSort([][]int{{75, 21, 13, 24, 8}, {24, 100, 40, 49, 62}}))
 	//rotate([][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}})
-	findOriginalArray([]int{2, 1, 2, 4, 2, 4})
+	//findOriginalArray([]int{2, 1, 2, 4, 2, 4})
+	fmt.Println(search([]int{-1, 0, 3, 5, 9, 12}, 9))
 }
